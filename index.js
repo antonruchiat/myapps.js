8fb8f302769e9f1738867760a94e4ac632310321
anton <anruc@outlook.co.id>
2018-09-25 16:47:09 +0700

    file js QUIZ INTERAKTIF

---
+++ index.js
@@ -0,0 +1,78 @@
+console.log("===========================================")
+console.log(" ")
+console.log("      GAMES QUIZ INTERAKTIF SEDERHANA ")
+console.log(" ")
+console.log("===========================================")
+console.log(" ")
+console.log(" ")
+var nomor = ['A','C','B','B','E','A','E','D','D','E'] ;
+
+var total = 0 ;
+var totalSalah = 0 ;
+
+if ( nomor[0] === 'A') {
+  var total1 = total + 1 ;
+} else {
+  var total1= totalSalah  ;
+}
+
+if ( nomor[1] === 'C'){
+  var total2 = total1 + 1 ;
+}else {
+  var total2 = totalSalah ;
+}
+
+if ( nomor[2] === 'B'){
+  var total3 = total2 + 1 ;
+}else {
+  var total3 = totalSalah  ;
+}
+
+if ( nomor[3] === 'B'){
+  var total4 = total3 + 1 ;
+}else {
+  var total4 = totalSalah ;
+}
+
+if ( nomor[4] === 'E'){
+  var total5 = total4 + 1 ;
+}else {
+  var total5 = totalSalah ;
+}
+
+if ( nomor[5] === 'A'){
+  var total6 = total5 + 1 ;
+}else {
+  var total6 = totalSalah ;
+}
+
+if ( nomor[6] === 'E') {
+  var total7 = total6 + 1 ;
+}else {
+  var total7 = totalSalah ;
+}
+
+if ( nomor[7] === 'D'){
+  var total8 = total7 + 1 ;
+}else {
+  var total8 = totalSalah ;
+}
+
+if ( nomor[8] === 'D'){
+  var total9 = total8 + 1 ;
+}else {
+  var total9 = totalSalah ;
+}
+
+if ( nomor[9] === 'E'){
+  var total10 = total9 + 1 ;
+}else {
+  var total10 = totalSalah;
+}
+
+var totalPo = total10 * 10 ;
+var TotalPOIN = totalPo  ;
+console.log('        Total Poin Kamu adalah: ' +TotalPOIN)
+
+console.log(" ")
+console.log("===========================================")
